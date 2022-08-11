1069c6afeeca6c4da2f1f8d75b9776d095016c57
Jaffar <jacepac264@gmail.com>
2022-08-11 18:41:17 +0300

    changed font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,19 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Teko", "serif"],
+    },
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px',
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {
       fontFamily: {
-        header: ["Bungee Outline", "sans-serif"]
+        header: ["Bungee Outline", "sans-serif"],
       },
       colors: {
-        meat: "#AE847E"
-      }
+        meat: "#AE847E",
+      },
     },
   },
   plugins: [],
